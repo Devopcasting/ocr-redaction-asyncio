ff3b75c12b898e8f9e6f8f21a47da2d25971f340
Devopscasting <devopcasting@gmail.com>
2023-10-08 03:17:02 +0530

    updated pan card p1 and p2

--- ocrr_engine.py
+++ ocrr_engine.py
@@ -5,6 +5,8 @@ import shutil
 import configparser
 from handlers.file_monitor import FileMonitor
 from handlers.identify_card import IdentifyCard
+from handlers.pan_card_p1 import PanCardPattern1Handler
+from handlers.pan_card_p2 import PanCardPattern2Handler
 from helpers.process_images import ProcessJPEGImages
 from ocrr_logging.ocrrlogging import OCRREngineLogging
 
@@ -40,16 +42,54 @@ async def identify_image(q):
             logger.error(f"ERR002")
             shutil.move(file_path, os.path.join(rejected_folder_path, file_name) )
         await asyncio.sleep(1)
-    
+
+# func: write xml data for pan card pattern 1
+async def write_xml_pan_card_p1(q):
+    while True:
+        file_path = await q.get()
+        file_name = os.path.basename(file_path)
+        if file_path is None:
+            break
+        # start: writing pan card pattern-1 info
+        write_pan_card_p1_data = PanCardPattern1Handler(file_path, xml_folder_path, redacted_folder_path).pancard_p1()
+        if write_pan_card_p1_data:
+            logger.info(f"Pan card xml data is ready for {file_name}")
+            q.task_done()
+        else:
+            logger.error(f"PANERR001")
+            shutil.move(file_path, os.path.join(rejected_folder_path, file_name))
+        await asyncio.sleep(1)
+
+# func: write xml data for pan card pattern 2
+async def write_xml_pan_card_p2(q):
+    while True:
+        file_path = await q.get()
+        file_name = os.path.basename(file_path)
+        if file_path is None:
+            break
+        # start: writing pan card pattern-2 info
+        write_pan_card_p2_data = PanCardPattern2Handler(file_path, xml_folder_path, redacted_folder_path).pancard_p2()
+        if write_pan_card_p2_data:
+            logger.info(f"Pan card xml data is ready for {file_name}")
+            q.task_done()
+        else:
+            logger.error(f"PANERR001")
+            shutil.move(file_path, os.path.join(rejected_folder_path, file_name))
+        await asyncio.sleep(1)
+
 # func: main ocrr-engine
 async def start_ocrr_engine():
     # define Queues
     upload_folder_q = asyncio.Queue()
     processed_folder_q = asyncio.Queue()
+    pan_card_p1_folder_q = asyncio.Queue()
+    pan_card_p2_folder_q = asyncio.Queue()
 
     #  monitor folders
     monitor_upload_folder = FileMonitor(upload_folder_q, upload_folder_path)
     monitor_processed_folder = FileMonitor(processed_folder_q, processed_folder_path)
+    monitor_pan_card_p1_folder = FileMonitor(pan_card_p1_folder_q, pan_card_p1_folder_path)
+    monitor_pan_card_p2_folder = FileMonitor(pan_card_p2_folder_q, pan_card_p2_folder_path)
 
     # func: wrap process_images in a async
     async def async_process_image(q):
@@ -59,15 +99,30 @@ async def start_ocrr_engine():
     async def async_identify_image(q):
         await identify_image(q)
 
+    # func: wrap write_xml_pan_card_p1 in a async
+    async def async_write_xml_pan_card_p1(q):
+        await write_xml_pan_card_p1(q)
+
+    # func: wrap write_xml_pan_card_p2 in a async
+    async def async_write_xml_pan_card_p2(q):
+        await write_xml_pan_card_p2(q)
+    
     # tasks
     upload_folder_task = asyncio.create_task(monitor_upload_folder.monitor())
     process_folder_task = asyncio.create_task(monitor_processed_folder.monitor())
+    pan_card_p1_folder_task = asyncio.create_task(monitor_pan_card_p1_folder.monitor())
+    pan_card_p2_folder_task = asyncio.create_task(monitor_pan_card_p2_folder.monitor())
 
     process_image_task = asyncio.create_task(async_process_image(upload_folder_q))
     identify_image_task = asyncio.create_task(async_identify_image(processed_folder_q))
+    write_xml_pan_card_p1_task = asyncio.create_task(async_write_xml_pan_card_p1(pan_card_p1_folder_q))
+    write_xml_pan_card_p2_task = asyncio.create_task(async_write_xml_pan_card_p2(pan_card_p2_folder_q))
     
     # gather all the tasks
-    await asyncio.gather(upload_folder_task, process_image_task, process_folder_task, identify_image_task)
+    await asyncio.gather(upload_folder_task, process_image_task, 
+                         process_folder_task, identify_image_task,
+                          pan_card_p1_folder_task, write_xml_pan_card_p1_task,
+                           pan_card_p2_folder_task, write_xml_pan_card_p2_task )
 
 if __name__ == '__main__':
 
@@ -87,5 +142,7 @@ if __name__ == '__main__':
     rejected_folder_path = config['Path']['rejected']
     pan_card_p1_folder_path = config['Path']['pancardp1']
     pan_card_p2_folder_path = config['Path']['pancardp2']
+    redacted_folder_path = config['Path']['redacted']
+    xml_folder_path = config['Path']['xml']
 
     asyncio.run(start_ocrr_engine())

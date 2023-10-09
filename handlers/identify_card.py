7a22dc9ffd684ab2866f8575ae17a32214efd21a
Devopscasting <devopcasting@gmail.com>
2023-10-09 18:21:26 +0530

    added aadhaar card identification

--- handlers/identify_card.py
+++ handlers/identify_card.py
@@ -4,14 +4,16 @@ import pytesseract
 from ocrr_logging.ocrrlogging import OCRREngineLogging
 from helpers.process_text import CleanText
 from helpers.identify_pan_card import IdentifyPanCard
+from helpers.identify_aadhaar_card import IdentifyAadhaarCard
 
 class IdentifyCard:
-    def __init__(self, image_path, pancard_p1, pancard_p2) -> None:
+    def __init__(self, image_path, pancard_p1, pancard_p2, aadhaarcard_front) -> None:
         # Get paths
         self.image_path = image_path
         self.file_name = os.path.basename(self.image_path)
         self.pancard_p1_path = pancard_p1
         self.pancard_p2_path = pancard_p2
+        self.aadhaarcard_front_path = aadhaarcard_front
 
         # Configure logger
         self.config = OCRREngineLogging()
@@ -25,8 +27,9 @@ class IdentifyCard:
         data_text = pytesseract.image_to_string(self.image_path, output_type=pytesseract.Output.DICT,config=self.tesseract_config)
         clean_text = CleanText(data_text).clean_text()
 
-        # Check for Pan card
+        # Check for Pan card and Aadhaar card
         pan_card = IdentifyPanCard(clean_text)
+        aadhaar_card = IdentifyAadhaarCard(clean_text)
 
         if pan_card.check_pan_card():
             if pan_card.identify_pan_card_pattern_1():
@@ -37,6 +40,13 @@ class IdentifyCard:
                 self.logger.info(f"Pan card {self.file_name} is of pattern 2")
                 shutil.move(self.image_path, os.path.join(self.pancard_p2_path, self.file_name))
                 return True
+        elif aadhaar_card.check_aadhaar_card():
+            if aadhaar_card.check_aadhaar_front():
+                self.logger.info(f"Front-side of Aadhaar card found in {self.file_name}")
+                shutil.move(self.image_path, os.path.join(self.aadhaarcard_front_path, self.file_name))
+                return True
+            else:
+                pass
         else:
             return False
 
